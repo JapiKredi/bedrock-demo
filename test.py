d751eb5b6cc46e5302d757706abf6f3b6d5b726d
JapiKredi <jasper.bongers@yahoo.com>
2024-06-22 22:00:21 +0800

    Update code

--- test.py
+++ test.py
@@ -1,2 +1,35 @@
 from langchain_aws import BedrockLLM as Bedrock
 from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+import boto3
+
+
+#bedrock client
+bedrock_client = boto3.client(
+    service_name = "bedrock",
+    region_name = "us-east-1"
+)
+
+model_id = "ai21.j2-ultra-v1"
+
+llm = Bedrock(
+    model_id = model_id,
+    client = bedrock_client
+)
+
+def my_model(user_prompt):
+    prompt = PromptTemplate(
+        input_variables=['user_prompt'],
+        template= "You are a chatbot. Please provide an answer for the following user prompt: {user_prompt}"
+    )
+
+    bedrock_chain = LLMChain(llm = llm, prompt = prompt)
+    response = bedrock_chain({'user_prompt': user_prompt})
+
+    return response
+
+
+
+user_prompt = "What is Python?"
+res = my_model(user_prompt)
+print(res['text'])
